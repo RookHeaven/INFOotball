af491e35e2f3decf3c2a3bd99e31b398fcbe6bdd
Roman Grachev <romagra92@gmail.com>
2024-02-02 20:08:39 +0500

    used useRef in the input field within the Search component

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,4 +1,4 @@
-import {useContext} from "react";
+import {useContext, useRef} from "react";
 import searchContext from "../../context/context.ts";
 
 import styles from './search.module.scss';
@@ -7,16 +7,23 @@ import SearchIcon from '../../assets/icons/search.svg?react';
 import Close from '../../assets/icons/close.svg?react';
 
 const Search = () => {
+  const inputRef = useRef();
   const {searchValue, setSearchValue} = useContext(searchContext);
+
+  const onClickClose = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  }
 
   return (
     <div className={styles.search}>
       <SearchIcon className={styles.search__icon}/>
-      <input value={searchValue}
+      <input ref={inputRef}
+             value={searchValue}
              onChange={(event) => setSearchValue(event.target.value)}
              className={styles.search__input}
              placeholder='Search club...'/>
-      {searchValue && <Close onClick={() => setSearchValue('')}
+      {searchValue && <Close onClick={onClickClose}
                              className={styles.search__closeIcon}/>}
     </div>
   )
