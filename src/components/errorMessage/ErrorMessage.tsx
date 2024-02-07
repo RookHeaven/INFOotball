0fbfd81c45ab114227bfcf4edf79161f22efd320
Roman Grachev <romagra92@gmail.com>
2024-02-07 19:28:56 +0500

    deleted ErrorMessage directory

--- src/components/errorMessage/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import img from './404.gif';
-
-const ErrorMessage = () => {
-  return (
-    <img src={img} style={{display: 'block', width: '700px', height: '700px', objectFit: 'contain', margin: '0 auto'}} alt='Error'/>
-  )
-}
-
-export default ErrorMessage;
--- src/pages/page404/Page404.tsx
+++ src/pages/page404/Page404.tsx
@@ -1,12 +1,13 @@
-import ErrorMessage from "../../components/errorMessage/ErrorMessage.tsx";
+import Button from "../../components/button/Button.tsx";
 
 import styles from './page404.module.scss'
-import Button from "../../components/button/Button.tsx";
+
+import img from "./404.gif";
 
 const Page404 = () => {
   return (
     <div className={styles.error}>
-      <ErrorMessage/>
+      <img className={styles.error__image} src={img} alt='Error'/>
       <p className={styles.error__text}>Page doesn't exist</p>
       <Button isBackButton={true}>Back to Main Page</Button>
     </div>
