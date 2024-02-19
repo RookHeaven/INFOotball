5a8205c0ded83521ecae579783dc29cbf757dec3
Roman Grachev <romagra92@gmail.com>
2024-02-19 17:17:06 +0500

    refactored README file

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# React + TypeScript + Vite
+# INFOotball
+
+"INFOotball" is an application for football enthusiasts who want to receive up-to-date information about their favorite football club. <br>
+Add clubs to your favorites, view detailed information about them. <br>
+And if you want to find a specific club, simply use the search bar or filtering.
+
+<div>
+    <img src="./INFOotball.png" alt="My Project GIF" width="3120" style="width: 100%; height: auto">
+</div>
+
+### ⚽ [View published version]()
+
+# Quick Start Guide
+Before you begin, ensure that **Node.js** is installed on your system.
+
+### Main Commands
+- Installation - `npm install`
+- Start Local Development Server - `npm run dev`
+- Build Project - `npm run build`
+- Lint Code - `npm run lint`
+- Preview Production Build Locally - `npm run preview`
+
+## React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/router/loaders/loaders.ts
+++ src/router/loaders/loaders.ts
@@ -1,19 +1,19 @@
 import {useHttp} from "../../hooks/http.hook.ts";
 
 import {TClubLoader} from './types.ts';
-import {Club, DataClub} from '../../@types/types.ts';
+import {IClub, IDataClub} from '../../@types/types.ts';
 
 import {_apiBase, _apiKey} from '../../constants/apiConstants.ts';
 import {_transformClubs} from '../../utils/formatUtils.ts';
 
-export const clubLoader = async ({params}: TClubLoader): Promise<Club | undefined> => {
+export const clubLoader = async ({params}: TClubLoader): Promise<IClub | undefined> => {
   // Todo deal with custom hook usage error
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const {request} = useHttp();
-  const clubs: DataClub[] =  await request(`${_apiBase}${_apiKey}`);
+  const clubs: IDataClub[] =  await request(`${_apiBase}${_apiKey}`);
 
   const id: string | undefined = params.id;
-  const club: Club | undefined = clubs.map(_transformClubs).find((c: Club) => c.id === id);
+  const club: IClub | undefined = clubs.map(_transformClubs).find((c: IClub) => c.id === id);
 
   return club;
 }
